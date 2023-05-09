7d07fbc11af37d716fe74991d78bd7f99b1279d3
TheSixthFinger <runwalparshva3@gmail.com>
2023-05-09 17:13:59 +0000

    Setting up express server and dotenv loader

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("ts-template is running.")
+import dotenv from "dotenv";
+import express from "express";
+
+dotenv.config({ path: `${__dirname}/.env` });
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.get(`/`, (req, res) => {
+  res.status(200).send({ message: "Server is up and healthy." });
+});
+
+app.listen(PORT, () => console.log(`server is listening on port: ${PORT}`));
